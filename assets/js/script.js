73c797e10b1dc54b402627a0881c3ca934985c49
unknown <jason.rettinger@gmail.com>
2022-08-25 18:09:53 -0400

    added final script/local storage/added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Display Current Date
-var todayDate = moment().format('dddd, MMM Do YYYY');
+var todayDate = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
@@ -37,4 +37,18 @@ $(document).ready(function () {
         })
     }
     
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
     
+    timeTracker();
+    })
+    
+    
+                
