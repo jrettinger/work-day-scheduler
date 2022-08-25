0cf04b8b91a4b55104f4ff7dac3c4f626796c915
unknown <jason.rettinger@gmail.com>
2022-08-25 18:01:37 -0400

    added classes for background based off current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,34 @@ $(document).ready(function () {
   $(".saveBtn").on("click", function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-
+// Savesin local storage
       localStorage.setItem(time, text);
     })
    
-  
+    function timeTracker() {
+        //Gets current time
+        var timeNow = moment().hour();
+    // Loops over the time blocks
+          $(".time-block").each(function () {
+          var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+    // Checks the current time and adds classes for backgrounds based off time
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+    
+            }
+        })
+    }
+    
+    
