2555cb5e4ed1df68cc96d39a76004aca9d9944a0
unknown <jason.rettinger@gmail.com>
2022-08-25 17:56:41 -0400

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,12 @@ var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
-    //Click Listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-// Saves in Local Storage
-        localStorage.setItem(time, text);
+    // saveBtn click listener  
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+      localStorage.setItem(time, text);
     })
-// Get Current Time
-    function timeTracker() {
-        var timeNow = moment().hour();
+   
+  
