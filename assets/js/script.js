05c3b00ce6c1431058facc9171c86d2072ceb2bf
unknown <jason.rettinger@gmail.com>
2022-10-14 09:33:42 -0400

    added new script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,110 @@
-// Display Current Date
-var todayDate = moment().format('dddd, MMMM Do YYYY');
-$("#currentDay").html(todayDate);
+const LS_KEY = "schedules";
 
-$(document).ready(function () {
-    // saveBtn click listener  
-  $(".saveBtn").on("click", function () {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-// Savesin local storage
-      localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //Gets current time
-        var timeNow = moment().hour();
-    // Loops over the time blocks
-          $(".time-block").each(function () {
-          var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-    // Checks the current time and adds classes for backgrounds based off time
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-    
-            }
-        })
-    }
-    
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-    
-    timeTracker();
-    })
-    
-    
-                
+function updateDateInfo() {
+  let today = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(today);
+}
+
+function getHourStatus(hour) {
+  let hourObj = {
+    "9AM": 9,
+    "10AM": 10,
+    "11AM": 11,
+    "12PM": 12,
+    "1PM": 13,
+    "2PM": 14,
+    "3PM": 15,
+    "4PM": 16,
+    "5PM": 17,
+  };
+  hour = hourObj[hour];
+  let currentHour = +moment().format("k");
+  if (hour < currentHour) {
+    return "past";
+  } else if (hour === currentHour) {
+    return "present";
+  } else {
+    return "future";
+  }
+}
+
+function getFromLS() {
+  const data = localStorage.getItem(LS_KEY);
+  if (!data) {
+    let obj = {
+      "9AM": "",
+      "10AM": "",
+      "11AM": "",
+      "12PM": "",
+      "1PM": "",
+      "2PM": "",
+      "3PM": "",
+      "4PM": "",
+      "5PM": "",
+    };
+    localStorage.setItem(LS_KEY, JSON.stringify(obj));
+    return obj;
+  } else {
+    return JSON.parse(data);
+  }
+}
+
+function saveToLS() {
+  let hourElement = $(this).siblings()[0];
+  let hour = $(hourElement).text();
+  let descElement = $(this).siblings()[1];
+  let desc = $(descElement).val();
+  let dataFromLS = getFromLS();
+  dataFromLS[hour] = desc;
+  localStorage.setItem(LS_KEY, JSON.stringify(dataFromLS));
+}
+
+function createSingleBlock(hour) {
+  let div = document.createElement("div");
+  $(div).addClass("time-block row");
+  let hourDiv = document.createElement("div");
+  $(hourDiv).addClass(
+    "hour col-md-1 justify-content-center align-items-center d-flex"
+  );
+  $(hourDiv).text(hour);
+  let desc = document.createElement("textarea");
+  $(desc).addClass("description col-md-10");
+  $(desc).addClass(getHourStatus(hour));
+  $(desc).attr("id", hour);
+  let saveBtn = document.createElement("button");
+  $(saveBtn).addClass("saveBtn col-md-1");
+  $(saveBtn).html("<i class='fa-solid fa-floppy-disk'></i>");
+  $(saveBtn).click(saveToLS);
+  $(div).append(hourDiv, desc, saveBtn);
+  return div;
+}
+
+function createTimeBlocks() {
+  let workHours = [
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM",
+  ];
+  workHours.forEach((hour) => $(".container").append(createSingleBlock(hour)));
+}
+
+function populateTimeBlocks() {
+  let data = getFromLS();
+  for (let block in data) {
+    $("#" + block).text(data[block]);
+  }
+}
+
+function main() {
+  updateDateInfo();
+  createTimeBlocks();
+  populateTimeBlocks();
+}
+
+$(document).ready(main);
